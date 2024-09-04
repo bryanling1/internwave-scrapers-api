26e4301942153370f58ec87db2e6eef831bfc17f
bryanling1 <bryanling107@gmail.com>
2024-09-04 14:05:22 -0400

    Add try catch to onStartScraping to handle thrown errors

--- src/ipc/onStartScraping/onStartScraping.ts
+++ src/ipc/onStartScraping/onStartScraping.ts
@@ -1,13 +1,25 @@
 import { reportDone } from "src/ipc/reportScraping/reportDone";
 import { IStartScrapingMessage, IStartScrapingPayload } from "src/ipc/types/message/variants/StartScrapingMessage";
 import { IScrapedJob } from "src/jobs/types/ScrapedJob";
+import { reportError } from "src/ipc/reportScraping/reportError";
 
 
 export const onStartScraping = (
-    callback:(payload: IStartScrapingPayload) => Promise<IScrapedJob[]>
+    callback:(payload: IStartScrapingPayload) => Promise<IScrapedJob[] | void>
 ) => {
     process.on("message", async (message: IStartScrapingMessage) => {
-        const jobs = await callback(message.payload);
-        reportDone(jobs)
+        try{
+            const jobs = await callback(message.payload);
+            if(jobs){
+                reportDone(jobs)
+            }
+        }catch(e){
+            if(e instanceof Error){
+                reportError(e.message)
+            }else if(typeof e === "string"){
+                reportError(e)
+            }
+        }
+        
     })
 } 
